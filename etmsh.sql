0bbe191501f19d285bdfd8cf3087482b3c87db70
FahrinNjh <fahrinnajah1711@gmail.com>
2025-01-03 14:37:38 +0700

    gitattributes

---
+++ etmsh.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jan 2025 pada 08.33
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `etmsh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `attendance_info`
+--
+
+CREATE TABLE `attendance_info` (
+  `aten_id` int(20) NOT NULL,
+  `atn_user_id` int(20) NOT NULL,
+  `in_time` varchar(200) DEFAULT NULL,
+  `out_time` varchar(150) DEFAULT NULL,
+  `total_duration` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `attendance_info`
+--
+
+INSERT INTO `attendance_info` (`aten_id`, `atn_user_id`, `in_time`, `out_time`, `total_duration`) VALUES
+(46, 28, '03-01-2025 13:26:36', NULL, NULL),
+(42, 1, '22-03-2021 22:01:43', '19-01-2023 20:42:36', '22:40:53'),
+(43, 1, '19-01-2023 20:45:04', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `task_info`
+--
+
+CREATE TABLE `task_info` (
+  `task_id` int(50) NOT NULL,
+  `t_title` varchar(120) NOT NULL,
+  `t_description` text DEFAULT NULL,
+  `t_start_time` varchar(100) DEFAULT NULL,
+  `t_end_time` varchar(100) DEFAULT NULL,
+  `t_user_id` int(20) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 = incomplete, 1 = In progress, 2 = complete'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `task_info`
+--
+
+INSERT INTO `task_info` (`task_id`, `t_title`, `t_description`, `t_start_time`, `t_end_time`, `t_user_id`, `status`) VALUES
+(27, 'CC', 'WEB', '2025-01-03 12:00', '2025-01-03 17:00', 28, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `user_id` int(20) NOT NULL,
+  `fullname` varchar(120) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `temp_password` varchar(100) DEFAULT NULL,
+  `user_role` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`user_id`, `fullname`, `username`, `email`, `password`, `temp_password`, `user_role`) VALUES
+(1, 'Admin', 'admin', 'admin@gmail.com', '85064efb60a9601805dcea56ec5402f7', NULL, 1),
+(28, 'FahrinNajah', 'fahrin', 'fahrin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '', 2),
+(29, 'WaskitoAgung', 'waskito', 'waskito@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '', 2),
+(30, 'MaulanaFurqon', 'alan', 'maulana@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '', 2),
+(31, 'MuhammadRaffi', 'raffi', 'raffi@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `attendance_info`
+--
+ALTER TABLE `attendance_info`
+  ADD PRIMARY KEY (`aten_id`);
+
+--
+-- Indeks untuk tabel `task_info`
+--
+ALTER TABLE `task_info`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indeks untuk tabel `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `attendance_info`
+--
+ALTER TABLE `attendance_info`
+  MODIFY `aten_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT untuk tabel `task_info`
+--
+ALTER TABLE `task_info`
+  MODIFY `task_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
