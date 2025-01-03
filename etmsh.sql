2566b672100a577456f007977ba66ce8dc41cf98
FahrinNjh <fahrinnajah1711@gmail.com>
2025-01-03 15:07:49 +0700

    gitattributes

--- etmsh.sql
+++ etmsh.sql
@@ -16,29 +16,24 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `etmsh`
---
+-- Hapus tabel jika sudah ada
+DROP TABLE IF EXISTS `attendance_info`;
+DROP TABLE IF EXISTS `task_info`;
+DROP TABLE IF EXISTS `tbl_admin`;
 
 -- --------------------------------------------------------
 
---
 -- Struktur dari tabel `attendance_info`
---
-
 CREATE TABLE `attendance_info` (
   `aten_id` int(20) NOT NULL,
   `atn_user_id` int(20) NOT NULL,
   `in_time` varchar(200) DEFAULT NULL,
   `out_time` varchar(150) DEFAULT NULL,
   `total_duration` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`aten_id`) -- Tetapkan primary key hanya di sini
+  PRIMARY KEY (`aten_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
 -- Dumping data untuk tabel `attendance_info`
---
-
 INSERT INTO `attendance_info` (`aten_id`, `atn_user_id`, `in_time`, `out_time`, `total_duration`) VALUES
 (46, 28, '03-01-2025 13:26:36', NULL, NULL),
 (42, 1, '22-03-2021 22:01:43', '19-01-2023 20:42:36', '22:40:53'),
@@ -46,10 +41,7 @@ INSERT INTO `attendance_info` (`aten_id`, `atn_user_id`, `in_time`, `out_time`, 
 
 -- --------------------------------------------------------
 
---
 -- Struktur dari tabel `task_info`
---
-
 CREATE TABLE `task_info` (
   `task_id` int(50) NOT NULL,
   `t_title` varchar(120) NOT NULL,
@@ -58,22 +50,16 @@ CREATE TABLE `task_info` (
   `t_end_time` varchar(100) DEFAULT NULL,
   `t_user_id` int(20) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 = incomplete, 1 = In progress, 2 = complete',
-  PRIMARY KEY (`task_id`) -- Tetapkan primary key hanya di sini
+  PRIMARY KEY (`task_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
 -- Dumping data untuk tabel `task_info`
---
-
 INSERT INTO `task_info` (`task_id`, `t_title`, `t_description`, `t_start_time`, `t_end_time`, `t_user_id`, `status`) VALUES
 (27, 'CC', 'WEB', '2025-01-03 12:00', '2025-01-03 17:00', 28, 0);
 
 -- --------------------------------------------------------
 
---
 -- Struktur dari tabel `tbl_admin`
---
-
 CREATE TABLE `tbl_admin` (
   `user_id` int(20) NOT NULL,
   `fullname` varchar(120) NOT NULL,
@@ -82,13 +68,10 @@ CREATE TABLE `tbl_admin` (
   `password` varchar(100) NOT NULL,
   `temp_password` varchar(100) DEFAULT NULL,
   `user_role` int(10) NOT NULL,
-  PRIMARY KEY (`user_id`) -- Tetapkan primary key hanya di sini
+  PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
 -- Dumping data untuk tabel `tbl_admin`
---
-
 INSERT INTO `tbl_admin` (`user_id`, `fullname`, `username`, `email`, `password`, `temp_password`, `user_role`) VALUES
 (1, 'Admin', 'admin', 'admin@gmail.com', '85064efb60a9601805dcea56ec5402f7', NULL, 1),
 (28, 'FahrinNajah', 'fahrin', 'fahrin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '', 2),
@@ -99,7 +82,6 @@ INSERT INTO `tbl_admin` (`user_id`, `fullname`, `username`, `email`, `password`,
 -- --------------------------------------------------------
 
 -- AUTO_INCREMENT untuk tabel yang dibuang
-
 ALTER TABLE `attendance_info`
   MODIFY `aten_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
