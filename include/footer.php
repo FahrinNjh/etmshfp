99b042045e742d11213294ef23564901b07465dd
FahrinNjh <fahrinnajah1711@gmail.com>
2025-01-03 14:40:54 +0700

    gitattributes

--- include/footer.php
+++ include/footer.php
@@ -2,19 +2,6 @@
     flex-wrap: wrap;
     justify-content: center; margin-left: -242px;"> CopyRight by Code Camp BD <?php echo "20".date("y")?> - Developer - <a href="https://developerrony.com"> MH RONY</a></h5>
 </div><!-- end of div.container-->
-<!-- /*
-
-Author: MH RONY
-CompunyName: Code Camp BD
-Facebook Profile: https://www.facebook.com/mh.rony.dhanvi
-GitHub Link: https://github.com/dev-mhrony
-Youtube Channel: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-Personal Website: https://developerrony.com
-Video Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-
-
-*/ -->
-
 </body>
 
 </html>
