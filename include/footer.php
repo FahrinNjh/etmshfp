29c658abfd482c6191f89cd800a31185187026dd
FahrinNjh <fahrinnajah1711@gmail.com>
2025-01-03 14:49:27 +0700

    gitattributes

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <h5 style="color: #38ff00;  display: flex;
     flex-wrap: wrap;
-    justify-content: center; margin-left: -242px;"> CopyRight by Code Camp BD <?php echo "20".date("y")?> - Developer - <a href="https://developerrony.com"> MH RONY</a></h5>
+    justify-content: center; margin-left: -242px;"> CopyRight<?php echo "20".date("y")?></a></h5>
 </div><!-- end of div.container-->
 </body>
 
